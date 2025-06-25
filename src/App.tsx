2098719148c78fc93dfbaef98e0be2a8c5fc5cda
ansh3108 <anshkumar9686@gmail.com>
2025-06-25 10:41:49 +0530

    'build' error

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React, { FC, useMemo } from "react";
+import { useMemo } from "react";
+import type { FC } from "react";
+
 import {
   ConnectionProvider,
   WalletProvider
 } from "@solana/wallet-adapter-react";
+
 import {
   WalletModalProvider,
   WalletMultiButton
 } from "@solana/wallet-adapter-react-ui";
+
 import {
   PhantomWalletAdapter
 } from "@solana/wallet-adapter-wallets";
+
 import { clusterApiUrl } from "@solana/web3.js";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
@@ -34,4 +39,3 @@ const App: FC = () => {
 };
 
 export default App;
-
